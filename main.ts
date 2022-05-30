aecf9f918e1c7d9cad3c06ac522fd68baf4705b2
tecnohuarte <106558201+tecnohuarte@users.noreply.github.com>
2022-05-30 22:50:14 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    timer = 0
+    basic.showIcon(IconNames.Happy)
+    soundExpression.happy.playUntilDone()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(timer)
+})
+input.onGesture(Gesture.Shake, function () {
+    timer = 0
+    basic.showIcon(IconNames.Surprised)
+    soundExpression.giggle.playUntilDone()
+})
+let timer = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    . . . . .
+    `)
+soundExpression.hello.playUntilDone()
+basic.forever(function () {
+    basic.pause(1000)
+    timer += 1
+    if (timer == 20) {
+        basic.showIcon(IconNames.Sad)
+        soundExpression.sad.playUntilDone()
+    }
+    if (timer == 30) {
+        basic.showIcon(IconNames.Asleep)
+        soundExpression.yawn.playUntilDone()
+    }
+    if (timer == 40) {
+        soundExpression.mysterious.playUntilDone()
+        music.setBuiltInSpeakerEnabled(false)
+        while (true) {
+            basic.showIcon(IconNames.Skull)
+        }
+    }
+})
